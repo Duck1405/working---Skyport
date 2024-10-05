e47b427a5c62e06aedd8585f7eaea488431fbfb7
ItzLoghotXD <121806387+ItzLoghotXD@users.noreply.github.com>
2024-10-05 17:03:57 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -78,6 +78,8 @@ execute() {
   elif [[ "$1" == "uninstall" ]]; then
     output "Uninstalling Skyport Panel/Daemon..."
     bash <(curl -s https://raw.githubusercontent.com/ItzLoghotXD/Skyport/main/installers/uninstall.sh)
+    output "Thankyou for using my script. I think that this script might have some issues, feel free to open an issue at https://github.com/ItzLoghotXD/Skyport/issues."
+    output "If you want to support me and if you can then please support me with crypto here(https://github.com/ItzLoghotXD/Skyport/blob/main/README.md#donationssupport)"
   elif [[ "$1" == "exit" ]]; then
     exit
   fi

1333a61273db7b7fb8bba18f2d5ae30c7e96a650
ItzLoghotXD <121806387+ItzLoghotXD@users.noreply.github.com>
2024-10-07 16:58:48 +0530

    Update deamon.sh

--- installers/deamon.sh
+++ installers/deamon.sh
@@ -70,47 +70,138 @@ if ! [ -x "$(command -v curl)" ]; then
   exit 1
 fi
 
-# Dependency Installation
-output "Installing dependencies"
+execute() {
+  if [[ "$1" == "sdeamon" ]]; then
 
-# Check if Docker is installed
-output "Installing Docker for the Skyport Daemon..."
-if ! command -v docker &> /dev/null; then
-    curl -sSL https://get.docker.com/ | CHANNEL=stable bash
-    success "Docker installed."
-else
-    output "Docker is already installed, skipping installation..."
-fi
+    # Dependency Installation
+    output "Installing dependencies"
 
-# Check if the NodeSource GPG key and repository are already set up
-output "Setting up Node.js repository..."
-if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
-    mkdir -p /etc/apt/keyrings
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-    success "Set up Node.js repository. Done!"
-else
-    output "Node.js repository is already set up, skipping..."
-fi
+    # Check if Docker is installed
+    output "Installing Docker for the Skyport Daemon..."
+    if ! command -v docker &> /dev/null; then
+        curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+        success "Docker installed."
+    else
+        output "Docker is already installed, skipping installation..."
+    fi
 
-apt update
-apt install -y nodejs git
+    # Check if the NodeSource GPG key and repository are already set up
+    output "Setting up Node.js repository..."
+    if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
+        mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+        success "Set up Node.js repository. Done!"
+    else
+        output "Node.js repository is already set up, skipping..."
+    fi
 
-# Deamon Installation
-output "Installing Skyport Deamon files"
-mkdir -p /etc/skyport
-cd /etc/skyport
+    apt update
+    apt install -y nodejs git
 
-if [ -d "skyportd" ]; then
-    output "Directory 'skyportd' already exists, skipping cloning."
-else
-    git clone https://github.com/skyportlabs/skyportd
-    success "Cloned deamon files."
-fi
+    # Deamon Installation
+    output "Installing Skyport Deamon files"
+    mkdir /etc/Skyport
+    cd /etc
 
-cd skyportd
-output "Installing packages."
-npm install && npm install axios
-success "Packages installed"
-success "Deamon installed. Create and configure a node in the panel, paste the token here, then use 'node /etc/skyport/skyportd/index.js' command as root in root folder to start the deamon."
-output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+    if [ -d "Skyport/deamon" ]; then
+        output "Directory 'deamon' already exists, skipping cloning."
+    else
+        mkdir tempspdir
+        cd tempspdir
+        git clone https://github.com/ItzLoghotXD/Skyport
+        cd Skyport
+        mv /etc/tempspdir/Skyport/deamon /etc/Skyport/deamon
+        cd /etc
+        rm -rf tempspdir
+        success "Cloned deamon files."
+    fi
+
+    cd /etc/Skyport/deamon
+    output "Installing packages."
+    npm install && npm install axios
+    success "Packages installed"
+    success "Deamon installed. Create and configure a node in the panel, paste the token here, then use 'cd /etc/skyport/skyportd/ && node .' command as root to start the deamon."
+    output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+
+  elif [[ "$1" == "deamon" ]]; then
+
+    # Dependency Installation
+    output "Installing dependencies"
+
+    # Check if Docker is installed
+    output "Installing Docker for the Skyport Daemon..."
+    if ! command -v docker &> /dev/null; then
+        curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+        success "Docker installed."
+    else
+        output "Docker is already installed, skipping installation..."
+    fi
+
+    # Check if the NodeSource GPG key and repository are already set up
+    output "Setting up Node.js repository..."
+    if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
+        mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+        success "Set up Node.js repository. Done!"
+    else
+        output "Node.js repository is already set up, skipping..."
+    fi
+
+    apt update
+    apt install -y nodejs git
+
+    # Deamon Installation
+    output "Installing Skyport Deamon files"
+    mkdir -p /etc/skyport
+    cd /etc/skyport
+
+    if [ -d "skyportd" ]; then
+        output "Directory 'skyportd' already exists, skipping cloning."
+    else
+        git clone https://github.com/skyportlabs/skyportd
+        success "Cloned deamon files."
+    fi
+
+    cd skyportd
+    output "Installing packages."
+    npm install && npm install axios
+    success "Packages installed"
+    success "Deamon installed. Create and configure a node in the panel, paste the token here, then use 'cd /etc/skyport/skyportd/ && node .' command as root to start the deamon."
+    output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+
+  elif [[ "$1" == "exit" ]]; then
+    exit
+  fi
+}
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install the latest stable version of deamon (unofficial)"
+    "Install latest version of deamon (official)"
+    "exit"
+  )
+
+  actions=(
+    "sdeamon"
+    "deamon"
+    "exit"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done

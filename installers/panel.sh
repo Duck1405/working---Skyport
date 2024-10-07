d2820d1f6c3728722172a73448adda5de9c4d3b7
ItzLoghotXD <121806387+ItzLoghotXD@users.noreply.github.com>
2024-10-07 16:59:24 +0530

    Update panel.sh

--- installers/panel.sh
+++ installers/panel.sh
@@ -70,47 +70,138 @@ if ! [ -x "$(command -v curl)" ]; then
   exit 1
 fi
 
-# Dependency Installation
-output "Installing dependencies"
-# Check if the NodeSource GPG key and repository are already set up
-output "Setting up Node.js repository..."
-if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
-    mkdir -p /etc/apt/keyrings
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-    success "Set up Node.js repository. Done!"
-else
-    output "Node.js repository is already set up, skipping..."
-fi
+execute() {
+  if [[ "$1" == "spanel" ]]; then
 
-apt update
-apt install -y nodejs git
+    # Dependency Installation
+    output "Installing dependencies"
+    # Check if the NodeSource GPG key and repository are already set up
+    output "Setting up Node.js repository..."
+    if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
+        mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+        success "Set up Node.js repository. Done!"
+    else
+        output "Node.js repository is already set up, skipping..."
+    fi
 
-# Panel Installation
-output "Installing Skyport Panel files"
-mkdir -p /etc/skyport
-cd /etc/skyport
+    apt update
+    apt install -y nodejs git
 
-if [ -d "panel" ]; then
-    output "Directory 'panel' already exists, skipping cloning."
-else
-    git clone https://github.com/skyportlabs/panel
-    success "Cloned panel files."
-fi
+    # Panel Installation
+    output "Installing Skyport Panel files"
+    mkdir /etc/Skyport
+    cd /etc
 
-cd panel
-output "Installing packages."
-npm install && npm install axios
-success "Packages installed"
-npm run seed
+    if [ -d "Skyport/panel" ]; then
+        output "Directory 'panel' already exists, skipping cloning."
+    else
+        mkdir tempspdir
+        cd tempspdir
+        git clone https://github.com/ItzLoghotXD/Skyport
+        cd Skyport
+        mv /etc/tempspdir/Skyport/panel /etc/Skyport/panel
+        cd /etc
+        rm -rf tempspdir
+        success "Cloned deamon files."
+    fi
 
-read -p "Do you want to create an account? (yes/no): " create_user
-if [[ "$create_user" == "yes" || "$create_user" == "y" ]]; then
-    npm run createUser
-    success "User account created."
-else
-    output "User account creation skipped."
-fi
+    cd /etc/Skyport/panel
+    output "Installing packages."
+    npm install && npm install axios
+    success "Packages installed"
+    npm run seed
 
-success "Panel installed. Use 'node /etc/skyport/panel/index.js' command as root in root folder to start the panel."
-output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+    read -p "Do you want to create an account? (yes/no): " create_user
+    if [[ "$create_user" == "yes" || "$create_user" == "y" ]]; then
+        npm run createUser
+        success "User account created."
+    else
+        output "User account creation skipped."
+    fi
+
+    success "Panel installed. Use 'cd /etc/Skyport/panel && node .' command as root to start the panel."
+    output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+
+  elif [[ "$1" == "panel" ]]; then
+
+    # Dependency Installation
+    output "Installing dependencies"
+    # Check if the NodeSource GPG key and repository are already set up
+    output "Setting up Node.js repository..."
+    if [ ! -f /etc/apt/keyrings/nodesource.gpg ]; then
+        mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+        success "Set up Node.js repository. Done!"
+    else
+        output "Node.js repository is already set up, skipping..."
+    fi
+
+    apt update
+    apt install -y nodejs git
+
+    # Panel Installation
+    output "Installing Skyport Panel files"
+    mkdir -p /etc/skyport
+    cd /etc/skyport
+
+    if [ -d "panel" ]; then
+        output "Directory 'panel' already exists, skipping cloning."
+    else
+        git clone https://github.com/skyportlabs/panel
+        success "Cloned panel files."
+    fi
+
+    cd panel
+    output "Installing packages."
+    npm install && npm install axios
+    success "Packages installed"
+    npm run seed
+
+    read -p "Do you want to create an account? (yes/no): " create_user
+    if [[ "$create_user" == "yes" || "$create_user" == "y" ]]; then
+        npm run createUser
+        success "User account created."
+    else
+        output "User account creation skipped."
+    fi
+
+    success "Panel installed. Use 'cd /etc/skyport/panel && node .' command as root to start the panel."
+    output "Open/Forword these ports - 3000, 3001, 3002, and 3003"
+
+  elif [[ "$1" == "exit" ]]; then
+    exit
+  fi
+}
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install the latest stable version of panel (unofficial)"
+    "Install latest version of panel (official)"
+    "exit"
+  )
+
+  actions=(
+    "spanel"
+    "panel"
+    "exit"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
